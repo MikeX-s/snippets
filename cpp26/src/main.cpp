ce9c7aac7794f397b63cfbab4f4c44160217a822
MikeX-s <michal.grzelczak@gmail.com>
2026-04-26 19:52:05 +0000

    Bleeding edge Fedora container for C++26!

---
+++ cpp26/src/main.cpp
@@ -0,0 +1,30 @@
+#include <meta>
+#include <print>
+#include <string>
+
+struct Person {
+  std::string name;
+  unsigned int age;
+  std::string email;
+};
+
+template <typename T> void debugPrint(const T &obj) {
+  std::println("{}:", std::meta::identifier_of(^^T));
+
+  template for (constexpr auto member :
+                std::define_static_array(std::meta::nonstatic_data_members_of(
+                    ^^T, std::meta::access_context::current()))) {
+
+    std::println(" {}: {}", std::meta::identifier_of(member), obj.[:member:]);
+  }
+}
+
+auto main() -> int {
+  Person bob{.name = "Bob", //
+             .age = 42,     //
+             .email = "a@b.com"};
+
+  debugPrint(bob);
+
+  return 0;
+}
